2412330469e11a8be69b403f7b0200f9c6806d92
Gire1 <168452060+Gire1@users.noreply.github.com>
2026-06-03 13:12:32 +0200

    Update Results.js

--- my-app/src/Results.js
+++ my-app/src/Results.js
@@ -11,7 +11,7 @@ export default function Results(props) {
       {props.results.phonetics.map(function (phonetic, index) {
         return (
           <div key={index}>
-            <Phonetics phonetic={phonetic} />
+            <Phonetic phonetic={phonetic} />
           </div>
         );
       })}
